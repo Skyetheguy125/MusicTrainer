ea239b0f306da9974eeb0b923ca55a79c45aaf9f
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-10-03 12:45:08 -0500

    creates stub file for MusicMath utility header

---
+++ MusicMath.py
@@ -0,0 +1,7 @@
+"""
+This is a common utility for containing all music-specific mathematical operations.
+Please include docstrings and use clear naming conventions, as other group members will likely be reüsing your methods.
+"""
+
+if __name__=="__main__":
+	print("Music & Math")
