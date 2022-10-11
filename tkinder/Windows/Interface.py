08c721a868c8bfa2adeee47b697a853aaeabdce6
Dylan <payned4@tamu.edu>
2022-10-11 16:24:49 -0500

    reduce lines of code. Added color. Added TAMU Background

--- tkinder/Windows/Interface.py
+++ tkinder/Windows/Interface.py
@@ -1,9 +1,36 @@
 from tkinter import *
 import tkinter.ttk as tk
+from PIL import ImageTk, Image
+
+def create_page(window):
+    #Create rows and collumn
+    temp = tk.Label(window,text='')
+    for i in range (1,6):
+        for j in range (1,7):
+            temp.grid(row=i,column=j)
+    
+    #Creates buttons      
+    b1 = Button(window,text='Tuner',width=12,pady=10,bg='light green').grid(row=5,column=1,columnspan=2,padx=20,pady=20)
+    b2 = Button(window,text='Train',width=12,pady=10,bg='light blue').grid(row=5,column=3,padx=20,pady=20)
+    b3 = Button(window,text='Stats',width=12,pady=10,bg='lavender').grid(row=5,column=5,columnspan=2,padx=20,pady=20)
+    
+    #Configures window to make middle row and column wider
+    window.grid_rowconfigure(2, weight=2)
+    window.grid_columnconfigure(3, weight=2)
+     
+def create_textboxes(window):
+        #Create Textbox
+    t1_label = Label(window,text='Bass:',width=11,bg='light green').grid(row=3,column=1,pady=3,columnspan=2)
+    t1 = Text(window,height=1,width=10).grid(row=4,column=1,columnspan=2)
+    t2_label = Label(window,text='Trebel:',width=11,bg='light blue').grid(row=3,pady=3,column=3)
+    t2 = Text(window,height=1,width=10).grid(row=4,column=3)
+    t3_label = Label(window,text='Alto:',width=11,bg='lavender').grid(row=3,column=5,pady=3,columnspan=2)
+    t3 = Text(window,height=1,width=10).grid(row=4,column=5,columnspan=2)
 
 def create_home_window():
+    #destroys other windows if open
     try:
-        trainer_window.destroy() #destroys other windows if open
+        trainer_window.destroy()
     except TclError:
         try:
             stats_window.destroy()
@@ -12,149 +39,83 @@ def create_home_window():
     except NameError:
         pass
 
-    global root
-    
-    #Create HomePage
+    global root #Creates new window
     root = Tk()
     root.title('Music Trainer')
     root.geometry('{}x{}'.format(800, 480)) #Width x Height
     
-    #Creating grid rows
-    r1 = tk.Label(root,text='')
-    r1.grid(row=1,column=1)
-    r2 = tk.Label(root,text='')
-    r2.grid(row=2,column=1)
-    r3 = tk.Label(root,text='')
-    r3.grid(row=3,column=1)
-    r5 = tk.Label(root,text='')
-    r5.grid(row=5,column=1)
+    #set Background
+    bg = PhotoImage(file='images/tamu_background.png')
+    my_label = Label(root,image=bg)
+    my_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    #Creates page, buttons, textboxes and header
+    create_page(root)                       #1st creates the grid and buttons
+    create_textboxes(root)                  #2nd Create Textboxes      
+    header = tk.Label(root,text='Tuner',font=("Arial",30,'bold'))
+    header.grid(row=1,column=3,columnspan=2)
     
-    #Create grid Columns
-    c1 = tk.Label(root,text='')
-    c1.grid(row=1,column=2)
-    c2 = tk.Label(root,text='')
-    c2.grid(row=1,column=2)
-    c3 = tk.Label(root,text='Tuner')
-    c3.grid(row=1,column=3,columnspan=2)
-    c4 = tk.Label(root,text='')
-    c4.grid(row=1,column=4)
-    c5 = tk.Label(root,text='')
-    c5.grid(row=1,column=5)
-    c6 = tk.Label(root,text='')
-    c6.grid(row=1,column=6)
-    
-    #Create Buttons
-    b1 = Button(root,text='Tuner',width=12,pady=10)
-    b1.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
-    b2 = Button(root,text='Train',width=12,pady=10, command=create_trainer_window)
-    b2.grid(row=5,column=3,padx=20,pady=20)
-    b3 = Button(root,text='Stats',width=12,pady=10,command=create_stats_window)
-    b3.grid(row=5,column=5,columnspan=2,padx=20,pady=20)
-    
-    #Create Textbox
-    t1_label = Label(root,text='Bass:')
-    t1_label.grid(row=3,column=1,columnspan=2)
-    t1 = Text(root,height=1,width=10)
-    t1.grid(row=4,column=1,columnspan=2)
-    t2_label = Label(root,text='Trebel:')
-    t2_label.grid(row=3,column=3)
-    t2 = Text(root,height=1,width=10)
-    t2.grid(row=4,column=3)
-    t3_label = Label(root,text='Alto:')
-    t3_label.grid(row=3,column=5,columnspan=2)
-    t3 = Text(root,height=1,width=10)
-    t3.grid(row=4,column=5,columnspan=2)
-    
-    #add image
-    my_image = PhotoImage(file ='images/sample2.png')
+    #adds in temporary image and text
+    my_file = 'images/sample2.png'
+    my_image = PhotoImage(file =my_file)
     image1 = Label(root,image=my_image)
     image1.grid(row=2,column=3)
-    image_label = Label(root,text='+/- 15chz')
+    image_label = Label(root,text='+/- 15chz',font=("Arial",12,'bold'))
     image_label.grid(row=2,column=4)
-    root.grid_rowconfigure(2, weight=2)
-    root.grid_columnconfigure(3, weight=2)
+            
+    #Adds Trainer and Stats button functionality
+    train_button = Button(root,text='Train',width=12,pady=10, bg='light blue',command=create_trainer_window)
+    train_button.grid(row=5,column=3,padx=20,pady=20)
+    stats_button = Button(root,text='Stats',width=12,pady=10,bg='lavender',command=create_stats_window)
+    stats_button.grid(row=5,column=5,columnspan=2,padx=20,pady=20)
     
+    #Run window
     root.mainloop()
 
-
 def create_trainer_window():
-    
+    #destroys other winsows if open
     try:
-        root.destroy() #destroys other winsows if open
+        root.destroy()
     except TclError as e:
         stats_window.destroy()
     
-    global trainer_window
-    
+    global trainer_window  #Creates new window
     trainer_window = Tk()
     trainer_window.title('Training')
     trainer_window.geometry('{}x{}'.format(800, 480)) #Width x Height
     
-        #Creating grid rows
-    r1 = tk.Label(trainer_window,text='')
-    r1.grid(row=1,column=1)
-    r2 = tk.Label(trainer_window,text='')
-    r2.grid(row=2,column=1)
-    r3 = tk.Label(trainer_window,text='')
-    r3.grid(row=3,column=1)
-    r5 = tk.Label(trainer_window,text='')
-    r5.grid(row=5,column=1)
+    #set Background
+    bg = PhotoImage(file='images/tamu_background.png')
+    my_label = Label(trainer_window,image=bg)
+    my_label.place(x=0, y=0, relwidth=1, relheight=1)
     
-    #Create grid Columns
-    c1 = tk.Label(trainer_window,text='')
-    c1.grid(row=1,column=2)
-    c2 = tk.Label(trainer_window,text='')
-    c2.grid(row=1,column=2)
-    c3 = tk.Label(trainer_window,text='Training')
-    c3.grid(row=1,column=3,columnspan=2)
-    c4 = tk.Label(trainer_window,text='')
-    c4.grid(row=1,column=4)
-    c5 = tk.Label(trainer_window,text='')
-    c5.grid(row=1,column=5)
-    c6 = tk.Label(trainer_window,text='')
-    c6.grid(row=1,column=6)
-    
-    #Create 3 Buttons
-    b1 = Button(trainer_window,text='Tuner',width=12,pady=10,command=create_home_window)
-    b1.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
-    b2 = Button(trainer_window,text='Train',width=12,pady=10)
-    b2.grid(row=5,column=3,padx=20,pady=20)
-    b3 = Button(trainer_window,text='Stats',width=12,pady=10,command=create_stats_window)
-    b3.grid(row=5,column=5,columnspan=2,padx=20,pady=20)
-    
-    #Create 3 Textbox
-    t1_label = Label(trainer_window,text='Bass:')
-    t1_label.grid(row=3,column=1,columnspan=2)
-    t1 = Text(trainer_window,height=1,width=10)
-    t1.grid(row=4,column=1,columnspan=2)
-    t2_label = Label(trainer_window,text='Trebel:')
-    t2_label.grid(row=3,column=3)
-    t2 = Text(trainer_window,height=1,width=10)
-    t2.grid(row=4,column=3)
-    t3_label = Label(trainer_window,text='Alto:')
-    t3_label.grid(row=3,column=5,columnspan=2)
-    t3 = Text(trainer_window,height=1,width=10)
-    t3.grid(row=4,column=5,columnspan=2)
+    #Create trainer page with header, buttons and Textboxes
+    create_page(trainer_window)
+    create_textboxes(trainer_window)                  #Create Textboxes
+    header = tk.Label(trainer_window,text='Training',font=("Arial",30,'bold'))
+    header.grid(row=1,column=3,columnspan=2)
     
     #adds in temporary image and text
-    image2 = PhotoImage(file ='images/train_icon.png')
-    image1 = Label(trainer_window,image=image2)
+    my_file = 'images/train_icon.png'
+    my_image = PhotoImage(file =my_file)
+    image1 = Label(trainer_window,image=my_image)
     image1.grid(row=2,column=3)
-    image_label = Label( trainer_window,text='+/- 15chz')
-    image_label.grid(row=2,column=4)
+    image_label = Label(trainer_window,text='+/- 15chz',font=("Arial",12,'bold'))
+    image_label.grid(row=2,column=4) 
     
-    #configures window
-    trainer_window.grid_rowconfigure(2, weight=2)
-    trainer_window.grid_columnconfigure(3, weight=2)
+    #Add button Tuner and Stats functionality
+    tuner_button = Button(trainer_window,text='Tuner',width=12,pady=10,bg='light green',command=create_home_window)
+    tuner_button.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
+    stats_button = Button(trainer_window,text='Stats',width=12,pady=10,bg='lavender',command=create_stats_window)
+    stats_button.grid(row=5,column=5,columnspan=2,padx=20,pady=20)
     
-    #run the window
+    #run window
     trainer_window.mainloop()
-    
-
+       
 def create_stats_window():
-    
+    #destroys other windows if open
     try:
-        root.destroy() #destroys other windows if open
+        root.destroy()
     except TclError as e:
         trainer_window.destroy()
         
@@ -165,57 +126,39 @@ def create_stats_window():
     stats_window.title('Your Statistics')
     stats_window.geometry('{}x{}'.format(800, 480)) #Width x Height
     
-    #Creating grid rows
-    r1 = tk.Label(stats_window,text='')
-    r1.grid(row=1,column=1)
-    r2 = tk.Label(stats_window,text='')
-    r2.grid(row=2,column=1)
-    r3 = tk.Label(stats_window,text='')
-    r3.grid(row=3,column=1)
-    r5 = tk.Label(stats_window,text='')
-    r5.grid(row=5,column=1)
+    #set Background
+    bg = PhotoImage(file='images/tamu_background.png')
+    my_label = Label(stats_window,image=bg)
+    my_label.place(x=0, y=0, relwidth=1, relheight=1)
     
-    #Create grid Columns
-    c1 = tk.Label(stats_window,text='')
-    c1.grid(row=1,column=2)
-    c2 = tk.Label(stats_window,text='')
-    c2.grid(row=1,column=2)
-    c3 = tk.Label(stats_window,text='Your Stats')
-    c3.grid(row=1,column=3,columnspan=2)
-    c4 = tk.Label(stats_window,text='')
-    c4.grid(row=1,column=4)
-    c5 = tk.Label(stats_window,text='')
-    c5.grid(row=1,column=5)
-    c6 = tk.Label(stats_window,text='')
-    c6.grid(row=1,column=6)
-    
-    #Create Progress Buttons
-    b1 = Button(stats_window,text='TODAY',width=12,pady=10)
-    b1.grid(row=4,column=1,columnspan=2,padx=20,pady=20)
-    b2 = Button(stats_window,text='WEEK',width=12,pady=10)
-    b2.grid(row=4,column=3,padx=20,pady=20)
-    b3 = Button(stats_window,text='MONTH',width=12,pady=10)
-    b3.grid(row=4,column=5,columnspan=2,padx=20,pady=20)
-    
-    #Create Buttons
-    b4 = Button(stats_window,text='Tuner',width=12,pady=10, command=create_home_window)
-    b4.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
-    b5 = Button(stats_window,text='Train',width=12,pady=10, command=create_trainer_window)
-    b5.grid(row=5,column=3,padx=20,pady=20)
-    b6 = Button(stats_window,text='Stats',width=12,pady=10)
-    b6.grid(row=5,column=5,columnspan=2,padx=20,pady=20)
+    #Creates page, buttons, and header
+    create_page(stats_window)                               #1st creates the grid and button      
+    header = tk.Label(stats_window,text='Your Progress',font=("Arial",25,'bold'))  #Header
+    header.grid(row=1,column=3,columnspan=2)
     
     #add image
-    my_image = PhotoImage(file ='images/stat_icon.png')
+    my_file = 'images/stat_icon.png'
+    my_image = PhotoImage(file =my_file)
     image1 = Label(stats_window,image=my_image)
     image1.grid(row=2,column=3)
-    image_label = Label(stats_window,text='Good Progress')
-    image_label.grid(row=2,column=4)
-    stats_window.grid_rowconfigure(2, weight=2)
-    stats_window.grid_columnconfigure(3, weight=2)
+    image_label = Label(stats_window,text='Great!',font=("Arial",15,'bold'))
+    image_label.grid(row=2,column=4)  
     
+    #Create Progress Buttons
+    b1 = Button(stats_window,text='TODAY',width=12,pady=10,bg='light green',font='bold')
+    b1.grid(row=4,column=1,columnspan=2,padx=20,pady=20)
+    b2 = Button(stats_window,text='WEEK',width=12,pady=10,bg='light blue',font='bold')
+    b2.grid(row=4,column=3,padx=20,pady=20)
+    b3 = Button(stats_window,text='MONTH',width=12,pady=10,bg='lavender',font='bold')
+    b3.grid(row=4,column=5,columnspan=2,padx=20,pady=20)
+    
+    #Add Tuner and Trainer Button functionality
+    tuner_button = Button(stats_window,text='Tuner',width=12,pady=10, bg='light green',command=create_home_window)
+    tuner_button.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
+    trainer_button = Button(stats_window,text='Train',width=12,pady=10, bg='light blue',command=create_trainer_window)
+    trainer_button.grid(row=5,column=3,padx=20,pady=20)
+    
+    #Run window
     stats_window.mainloop()
-  
 
-
-create_home_window()
+create_home_window() #Start program
