FFT could be working, but sample data isn't in correct units

Hardware/test fft.py
@@ -1,11 +1,11 @@
 import scipy.fft as fft
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io import wavfile
 import pandas as pd
+import math
 
-SAMPLE_RATE = 20  # Hertz
-DURATION = 0.05  # Seconds
+SAMPLE_RATE = 33.33  # Hertz
+DURATION = 3  # Seconds
 
 #Use Pandas to read sample csv
 df = pd.read_csv('sample.csv', header=None)
@@ -15,10 +15,20 @@ df = pd.read_csv('sample.csv', header=None)
 result = df[0].tolist()
 #print(result)
 
-#Plot List
-plt.plot(result)
-plt.xlabel('Time')
-plt.ylabel('Amplitude')
-plt.title('Boop')
+#Attempt FFT
+N = math.ceil(SAMPLE_RATE * DURATION)
+yf = fft.rfft(result)
+xf = fft.rfftfreq(N, 1 / SAMPLE_RATE)
+plt.plot(xf, np.abs(yf))
+plt.xlabel("Frequency")
+plt.ylabel("Stuff")
 plt.grid()
 plt.show()
+
+#Plot data
+#plt.plot(result)
+#plt.xlabel('Time')
+#plt.ylabel('Amplitude')
+#plt.title('Boop')
+#plt.grid()
+#plt.show()