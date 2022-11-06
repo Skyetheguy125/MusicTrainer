72648c9ce9c29dd826dcc18eef6c5a0452113973
xly100 <xanderlybeta@gmail.com>
2022-11-06 11:03:11 -0600

    Added new sample csv, changed interval in graph

--- Hardware/test fft.py
+++ Hardware/test fft.py
@@ -4,12 +4,13 @@ import numpy as np
 import pandas as pd
 import math
 
-SAMPLE_RATE = 33.33  # Hertz
+SAMPLE_RATE = 66.66  # Hertz
 DURATION = 3  # Seconds
+DATA_POINTS = 200  # Samples
 
 #Use Pandas to read sample csv
-df = pd.read_csv('sample.csv', header=None)
-#print(df)
+df = pd.read_csv('sample200.csv', header=None)
+#rint(df)
 
 #Take channel 0 only and convert to a list
 result = df[0].tolist()
@@ -18,17 +19,9 @@ result = df[0].tolist()
 #Attempt FFT
 N = math.ceil(SAMPLE_RATE * DURATION)
 yf = fft.rfft(result)
-xf = fft.rfftfreq(N, 1 / SAMPLE_RATE)
+xf = fft.rfftfreq(N, (SAMPLE_RATE / DATA_POINTS))
 plt.plot(xf, np.abs(yf))
 plt.xlabel("Frequency")
 plt.ylabel("Stuff")
 plt.grid()
 plt.show()
-
-#Plot data
-#plt.plot(result)
-#plt.xlabel('Time')
-#plt.ylabel('Amplitude')
-#plt.title('Boop')
-#plt.grid()
-#plt.show()
