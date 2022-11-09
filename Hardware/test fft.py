8b314e451f8b348179767b9da9ef8ffbb36eeb33
xly100 <xanderlybeta@gmail.com>
2022-11-09 13:42:47 -0600

    new samples, changes to how xf is calc'd in test fft

--- Hardware/test fft.py
+++ Hardware/test fft.py
@@ -5,25 +5,26 @@ import pandas as pd
 import math
 
 #(SAMPLE_RATE * DURATION) MUST EQUAL DATA_POINTS
-SAMPLE_RATE = 25  # Hertz
-DURATION = 8  # Seconds
-DATA_POINTS = 200  # Samples
+DATA_POINTS = 1000  # Samples
+DURATION = .112  # Seconds
+SAMPLE_RATE = (DATA_POINTS / DURATION)  # Hertz
 
 #Use Pandas to read sample csv
-df = pd.read_csv('sample860.csv', header=None)
+df = pd.read_csv('1000samples_1pluck.csv', header=None)
 #print(df)
 
 #Take channel 0 only and convert to a list
 result = df[0].tolist()
-#print(result)
+#print(result[-1])
 
 #Attempt FFT
 N = math.ceil(SAMPLE_RATE * DURATION)
 B = (SAMPLE_RATE / DATA_POINTS)
 B2 = (1 / SAMPLE_RATE)
 yf = fft.rfft(result)
-xf = fft.rfftfreq(N, B)
-print(len(xf))
+xf = np.arange(0,501)
+xf = xf * B
+print(xf)
 plt.plot(xf[1:], np.abs(yf[1:]))
 plt.title("Test FFT 1")
 plt.xlabel("Frequency")
