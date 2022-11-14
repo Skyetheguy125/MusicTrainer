db76213bdd60e584a553690a7bd8751114dfffd7
xly100 <xanderlybeta@gmail.com>
2022-11-14 13:15:40 -0600

    Michael added filter to code before plotting, changed window of graph to target frequencies

--- Hardware/test fft.py
+++ Hardware/test fft.py
@@ -1,4 +1,5 @@
 import scipy.fft as fft
+import scipy.signal as signal
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -15,17 +16,22 @@ df = pd.read_csv('1000samples_1pluck.csv', header=None)
 
 #Take channel 0 only and convert to a list
 result = df[0].tolist()
-#print(result[-1])
+
 
 #Attempt FFT
-N = math.ceil(SAMPLE_RATE * DURATION)
 B = (SAMPLE_RATE / DATA_POINTS)
-B2 = (1 / SAMPLE_RATE)
 yf = fft.rfft(result)
+yf = [0 if x < 22 else yf[x] for x in range(len(yf))] #zero out low frequency
+q1,q3 = np.percentile(yf[22:], [25,75]) #find quartiles (ignoring low frequency)
+iqr = q3 - q1 #find interquartile range
+fence_high = q3 + (1.5 * iqr) #statistical cutoff for outliers (high end)
+fence_low = q1 - (1.5 * iqr) #statistical cutoff for outliers (low end)
+yf = [0 if fence_low <= i <= fence_high else i for i in yf] #if the value is an outlier, keep it; otherwise, zero it
 xf = np.arange(0,501)
 xf = xf * B
-print(xf)
-plt.plot(xf[1:], np.abs(yf[1:]))
+
+#Plot FFT
+plt.plot(xf[22:147], np.abs(yf[22:147]))
 plt.title("Test FFT 1")
 plt.xlabel("Frequency")
 plt.ylabel("Amplitude")
