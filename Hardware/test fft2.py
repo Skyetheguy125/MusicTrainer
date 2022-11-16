7af319f82644f297429b41838ca688fc7264c073
xly100 <xanderlybeta@gmail.com>
2022-11-16 13:40:19 -0600

    Changed fft2 and fft3 to store newer methods

--- Hardware/test fft2.py
+++ Hardware/test fft2.py
@@ -1,18 +1,67 @@
-#https://realpython.com/python-scipy-fft/
+import scipy.fft as fft
+import scipy.signal as signal
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
+import math
+import parabolic
+import time
 
-SAMPLE_RATE = 44100  # Hertz
-DURATION = 5  # Seconds
+def freq_from_fft(sig, fs):
+    """
+    Estimate frequency from peak of FFT
+    """
+    # Compute Fourier transform of windowed signal
+    windowed = sig * signal.blackmanharris(len(sig))
+    f = fft.rfft(windowed)
 
-def generate_sine_wave(freq, sample_rate, duration):
-    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-    frequencies = x * freq
-    # 2pi because np.sin takes radians
-    y = np.sin((2 * np.pi) * frequencies)
-    return x, y
+    # Find the peak and interpolate to get a more accurate peak
+    i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
+    #true_i = parabolic(math.log(abs(f)), i)[0]
 
-# Generate a 2 hertz sine wave that lasts for 5 seconds
-x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
-plt.plot(x, y)
-plt.show()
+    # Convert to equivalent frequency
+    return fs * i / len(windowed) #swap between i and true_i
+
+def freq_from_HPS(sig, fs):
+    """
+    Estimate frequency using harmonic product spectrum (HPS)
+    """
+    windowed = sig * signal.blackmanharris(len(sig))
+
+    from pylab import subplot, plot, log, copy, show
+
+    # harmonic product spectrum:
+    c = abs(fft.rfft(windowed))
+    maxharms = 8
+    subplot(maxharms, 1, 1)
+    plot(log(c))
+    for x in range(2, maxharms):
+        a = copy(c[::x])  # Should average or maximum instead of decimating
+        # max(c[::x],c[1::x],c[2::x],...)
+        c = c[:len(a)]
+        i = np.argmax(abs(c))
+        #true_i = parabolic(abs(c), i)[0]
+        print('Pass %d: %f Hz' % (x, fs * i / len(windowed)))
+        c *= a
+        subplot(maxharms, 1, x)
+        plot(log(c))
+    show()
+
+
+#(SAMPLE_RATE * DURATION) MUST EQUAL DATA_POINTS
+DATA_POINTS = 1000  # Samples
+DURATION = .112  # Seconds
+SAMPLE_RATE = (DATA_POINTS / DURATION)  # Hertz
+
+#Use Pandas to read sample csv
+df = pd.read_csv('1000samples_1pluck.csv', header=None)
+#print(df)
+
+#Take channel 0 only and convert to a list
+result = df[0].tolist()
+
+#Attempt function
+print('Calculating frequency from FFT:', end=' ')
+start_time = time.time()
+print('%f Hz' % freq_from_fft(result, SAMPLE_RATE))
+print('Time elapsed: %.3f s\n' % (time.time() - start_time))
--- Hardware/test fft3.py
+++ Hardware/test fft3.py
@@ -1,43 +1,19 @@
-#https://towardsdatascience.com/music-in-python-2f054deb41f4
 import numpy as np
 from scipy.io import wavfile
+from scipy import signal
 import matplotlib.pyplot as plt
 
+def freq_from_fft(sig, fs):
+    """
+    Estimate frequency from peak of FFT
+    """
+    # Compute Fourier transform of windowed signal
+    windowed = sig * blackmanharris(len(sig))
+    f = rfft(windowed)
 
-def get_piano_notes():
-    # White keys are in Uppercase and black keys (sharps) are in lowercase
-    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
-    base_freq = 440  # Frequency of Note A4
-    keys = np.array([x + str(y) for y in range(0, 9) for x in octave])
-    # Trim to standard 88 keys
-    start = np.where(keys == 'A0')[0][0]
-    end = np.where(keys == 'C8')[0][0]
-    keys = keys[start:end + 1]
+    # Find the peak and interpolate to get a more accurate peak
+    i = argmax(abs(f))  # Just use this for less-accurate, naive version
+    true_i = parabolic(log(abs(f)), i)[0]
 
-    note_freqs = dict(zip(keys, [2 ** ((n + 1 - 49) / 12) * base_freq for n in range(len(keys))]))
-    note_freqs[''] = 0.0  # stop
-    return note_freqs
-
-def get_sine_wave(frequency, duration, sample_rate=44100, amplitude=4096):
-    t = np.linspace(0, duration, int(sample_rate*duration)) # Time axis
-    wave = amplitude*np.sin(2*np.pi*frequency*t)
-    return wave
-
-# Get middle C frequency
-note_freqs = get_piano_notes()
-frequency = note_freqs['C4']
-
-# Pure sine wave
-sine_wave = get_sine_wave(frequency, duration=2, amplitude=2048)
-wavfile.write('pure_c.wav', rate=44100, data=sine_wave.astype(np.int16))
-
-# Load data from wav file
-sample_rate, middle_c = wavfile.read('pure_c.wav')
-
-# Plot sound wave
-plt.plot(middle_c[500:2500])
-plt.xlabel('Time')
-plt.ylabel('Amplitude')
-plt.title('Sound Wave of Pure C')
-plt.grid()
-plt.show()
+    # Convert to equivalent frequency
+    return fs * true_i / len(windowed)
