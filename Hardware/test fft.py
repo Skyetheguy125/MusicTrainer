b456f15652ec2df4c2dbb603d47c9b8ac75d4da5
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-11-16 11:57:59 -0600

    Outlier filtering

--- Hardware/test fft.py
+++ Hardware/test fft.py
@@ -1,4 +1,5 @@
 import scipy.fft as fft
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -10,7 +11,7 @@ DURATION = .112  # Seconds
 SAMPLE_RATE = (DATA_POINTS / DURATION)  # Hertz
 
 #Use Pandas to read sample csv
-df = pd.read_csv('1000samples_1pluck.csv', header=None)
+df = pd.read_csv('Hardware/1000samples_1pluck.csv', header=None) #G4 / 392 Hz
 #print(df)
 
 #Take channel 0 only and convert to a list
@@ -22,6 +23,13 @@ N = math.ceil(SAMPLE_RATE * DURATION)
 B = (SAMPLE_RATE / DATA_POINTS)
 B2 = (1 / SAMPLE_RATE)
 yf = fft.rfft(result)
+l = np.min(np.abs(yf))
+yf = [l if x < 22 else yf[x] for x in range(len(yf))] #zero out low frequency
+q1,q3 = np.percentile(yf[22:], [25,75]) #find quartiles (ignoring low frequency)
+iqr = q3 - q1 #find interquartile range
+fence_high = q3 + (1.5 * iqr) #statistical cutoff for outliers (high end)
+fence_low = q1 - (1.5 * iqr) #statistical cutoff for outliers (low end)
+yf = [0 if fence_low <= i <= fence_high else i for i in yf] #if the value is an outlier, keep it; otherwise, zero it
 xf = np.arange(0,501)
 xf = xf * B
 print(xf)
