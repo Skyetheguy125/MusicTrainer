aed94bbf249d57a908f961b7033d348192b0ce73
xly100 <xanderlybeta@gmail.com>
2022-11-16 14:01:26 -0600

    Changed fft2 to account for 0th bin maximum

--- Hardware/test fft2.py
+++ Hardware/test fft2.py
@@ -1,5 +1,5 @@
 import scipy.fft as fft
-import scipy.signal as signal
+import scipy.signal.windows as win
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,11 +12,15 @@ def freq_from_fft(sig, fs):
     Estimate frequency from peak of FFT
     """
     # Compute Fourier transform of windowed signal
-    windowed = sig * signal.blackmanharris(len(sig))
+    windowed = sig * win.blackmanharris(len(sig))
+    #print(len(sig))
+    #print(len(windowed))
     f = fft.rfft(windowed)
+    #print(f)
 
     # Find the peak and interpolate to get a more accurate peak
-    i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
+    i = np.argmax(abs(f[22:135]))  # Just use this for less-accurate, naive version
+    print("Max: ", i)
     #true_i = parabolic(math.log(abs(f)), i)[0]
 
     # Convert to equivalent frequency
@@ -26,12 +30,12 @@ def freq_from_HPS(sig, fs):
     """
     Estimate frequency using harmonic product spectrum (HPS)
     """
-    windowed = sig * signal.blackmanharris(len(sig))
+    windowed = sig * win.blackmanharris(len(sig))
 
     from pylab import subplot, plot, log, copy, show
 
     # harmonic product spectrum:
-    c = abs(fft.rfft(windowed))
+    c = abs(fft.rfft(windowed)[22:135])
     maxharms = 8
     subplot(maxharms, 1, 1)
     plot(log(c))
