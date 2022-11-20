b7132dbded5a6aa8d2439cba53c3c8ca136718cd
xly100 <xanderlybeta@gmail.com>
2022-11-20 16:00:09 -0600

    New samples (all samples moved to new directory), changed pd.read_csv line accordingly, changed function call in fft2

--- Hardware/test fft2.py
+++ Hardware/test fft2.py
@@ -58,14 +58,19 @@ DURATION = .112  # Seconds
 SAMPLE_RATE = (DATA_POINTS / DURATION)  # Hertz
 
 #Use Pandas to read sample csv
-df = pd.read_csv('1000samples_1pluck.csv', header=None)
+df = pd.read_csv('Samples/1000samples_1pluck.csv', header=None)
 #print(df)
 
 #Take channel 0 only and convert to a list
 result = df[0].tolist()
+print("Length: ", len(result))
 
 #Attempt function
 print('Calculating frequency from FFT:', end=' ')
 start_time = time.time()
-print('%f Hz' % freq_from_fft(result, SAMPLE_RATE))
+#print('%f Hz' % freq_from_fft(result, SAMPLE_RATE))
+f = fft.rfft(result)
+i = np.argmax(abs(f[22:135]))  # Just use this for less-accurate, naive version
+print("Max: ", i)
+print('%f Hz' % ((SAMPLE_RATE / len(result)) * i)) #swap between i and true_i
 print('Time elapsed: %.3f s\n' % (time.time() - start_time))
