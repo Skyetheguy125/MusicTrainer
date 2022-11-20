c811af1c6b744619204ce61a39523e10b3663b09
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-11-20 12:51:27 -0600

    Changes filtering algorithm Algorithm now inspects power (square of magnitude) rather than complex value or magnitude. Difference between outlier power values is more pronounced than working with just the magnitude. Algorithm now sets all values outside the range (196,1205) Hz to zero, and ignores them for further calculations. Plot now shoes all data, outlier (filtered) data overlayed onto it, and a horizontal line representing the cutoff for outlier range.

--- Hardware/test fft.py
+++ Hardware/test fft.py
@@ -11,7 +11,7 @@ DURATION = .112  # Seconds
 SAMPLE_RATE = (DATA_POINTS / DURATION)  # Hertz
 
 #Use Pandas to read sample csv
-df = pd.read_csv('Hardware/1000samples_1pluck.csv', header=None) #G4 / 392 Hz
+df = pd.read_csv('Hardware/2ndstring.csv', header=None) #G4 / 392 Hz
 #print(df)
 
 #Take channel 0 only and convert to a list
@@ -23,19 +23,23 @@ N = math.ceil(SAMPLE_RATE * DURATION)
 B = (SAMPLE_RATE / DATA_POINTS)
 B2 = (1 / SAMPLE_RATE)
 yf = fft.rfft(result)
-l = np.min(np.abs(yf))
-yf = [l if x < 22 else yf[x] for x in range(len(yf))] #zero out low frequency
-q1,q3 = np.percentile(yf[22:], [25,75]) #find quartiles (ignoring low frequency)
+yf = np.power(np.abs(yf),2) #work with power rather than complex value or magnitude
+yf = [0 if x < 22 or x > 135 else yf[x] for x in range(len(yf))] #zero-out out-of-range frequencies
+q1,q3 = np.percentile(yf[22:135], [25,75]) #find quartiles for in-range frequencies
 iqr = q3 - q1 #find interquartile range
 fence_high = q3 + (1.5 * iqr) #statistical cutoff for outliers (high end)
-fence_low = q1 - (1.5 * iqr) #statistical cutoff for outliers (low end)
-yf = [0 if fence_low <= i <= fence_high else i for i in yf] #if the value is an outlier, keep it; otherwise, zero it
+#fence_low = q1 - (1.5 * iqr) #statistical cutoff for outliers (low end)
+yf1 = [0 if i <= fence_high else i for i in yf] #if the value is an outlier, keep it; otherwise, zero it
 xf = np.arange(0,501)
 xf = xf * B
 print(xf)
-plt.plot(xf[1:], np.abs(yf[1:]))
+plt.plot(xf, yf) #plot all data in range
+plt.plot(xf,yf1) #plot only outliers in range (overlaid)
+plt.axhline(fence_high) #visual representation of the cutoff for being an outlier
+#plt.axhline(q3)
 plt.title("Test FFT 1")
 plt.xlabel("Frequency")
 plt.ylabel("Amplitude")
 plt.grid()
 plt.show()
+print(yf)
