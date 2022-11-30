4a3c2c9cc9c9eca1ab4bc624f790271512eaf2d3
Sean <sean.yessam@gmail.com>
2022-11-29 20:28:19 -0600

    Updated serialRead.py with artifact handling

--- serialRead.py
+++ serialRead.py
@@ -3,20 +3,29 @@ import time
 import csv
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
 
     numSamples = 0
     start = time.monotonic()
-    with open('output.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        while numSamples < 1000:
-            if ser.in_waiting > -0:
-                line = ser.readline().rstrip()
-                decodeLine = line.decode('UTF-8',errors='ignore')
-                numSamples += 1
-                csvwriter.writerow(decodeLine)
+
+    f = open('output.csv', 'w')
+    writer = csv.writer(f)
+
+
+    while numSamples < 10000:
+        if ser.in_waiting > 0:
+            line = ser.readline().rstrip()
+            decodeLine = line.decode('UTF-8',errors='ignore')
+            numSamples += 1
+            # decodeLine.translate({ord('"'): None})
+            if (decodeLine.isdigit()):
+                writer.writerow([decodeLine])
+            else:
+                writer.writerow([0])
+            # writer.writerow(line)
 
     end = time.monotonic()
     runtime = end - start
     print(runtime)
+    f.close()
