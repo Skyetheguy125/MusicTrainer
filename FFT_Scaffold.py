e913ad6ff71c9c0de40f9dde90fa6a485ce80b19
Skyetheguy125 <skyetheguy125@tamu.edu>
2022-11-30 11:06:45 -0600

    modified for pi

--- FFT_Scaffold.py
+++ FFT_Scaffold.py
@@ -20,7 +20,7 @@ class FFT_Scaffold:
 		self._signal = self._signalReader()
 		self._last_value = None
 		self.DATA_POINTS = 5000  # Samples 
-		self.SAMPLE_RATE = 3000  # Hertz
+		self.SAMPLE_RATE = 2400  # Hertz
 		
 	def _signalReader(self):
 		"""returns a signal by reading the csv that is updated"""
--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-if __name__ == "__main__":
-    window = tk.Tk()
-    greeting = tk.Label(text="Hello, Tkinter")
-    greeting.pack()
-    window.mainloop()
-
